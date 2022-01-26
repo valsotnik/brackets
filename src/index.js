8e0c422615af0dfe159929c46ba685eeef4def42
valsotnik <cmvaltime@gmail.com>
2022-01-26 15:01:09 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+	let arr = bracketsConfig.map(a => a.join(''));
+	for (let i = 0; i < arr.length;) {
+		if (str.includes(arr[i])) {
+			str = str.replace(arr[i], '');
+			i = 0;
+		}
+		else i++
+	}
+	return (str.length === 0)
 }
